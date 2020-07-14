af39f2113d9784c3283e744753bfa94843836827
masbc666 <63325598+masbc666@users.noreply.github.com>
2020-07-14 10:18:48 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,7 +1,12 @@
 read -e -p "路径:" path
 read -e -p "输入文件名:" file_name
 read -e -p "输出文件名:" output_file_name
-cd $path
+if [ $path=="" ]
+then
+	echo 无路径指向,默认在c.sh同目录下查找文件
+else
+	cd $path
+fi
 echo 编译......
 g++ -fno-asm -Wall -lm --static -std=c++11 -o $output_file_name $file_name
 echo 编译结束.
